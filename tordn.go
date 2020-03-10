7597420618a19f20709e088bdb3b8c0e1987ea08
kawser <mkawserm@gmail.com>
2020-03-11 03:56:48 +0600

    MINOR REFACTORING

--- tordn.go
+++ tordn.go
@@ -1,5 +1,7 @@
 package tordn
 
+// TOR domain name generator interface
 type TORDomainNameGenerator interface {
-	GenerateTORDomainName(secretKey []byte) ([]byte, []byte, []byte, error)
+	// Generate tor domain name using the secret key
+	GenerateTORDomainName(secretKey []byte) (publicKey []byte, privateKey []byte, onionAddress []byte, err error)
 }
